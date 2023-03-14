7ece032d83f1b4e3505b3ffc451e9c8c0a269c9f
Mehdi Qaos <mehdi.qaos@gmail.com>
2023-03-14 10:16:00 +0100

    a

---
+++ src/assets/data/data.js
@@ -0,0 +1,131 @@
+const allQuestions = [
+    {
+        question: "What does PHP stand for?",
+        choices: [
+            "Personal Hypertext Processor",
+            "PHP: Hypertext Preprocessor",
+            "Private Home Page",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "2"
+    },
+    {
+        question: "How do you write `Hello World` in PHP?",
+        choices: [
+            "Document.Write('Hello Wordld');",
+            "echo 'Hello World';",
+            "'Hello World'",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "2"
+    },
+    {
+        question: "How do you get information from a form that is submitted using the 'get' method?",
+        choices: [
+            "Request.Form",
+            "Request.QueryString",
+            "$_GET[]",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "3"
+    },
+    {
+        question: "Include files must have file extension *.inc?",
+        choices: [
+            "True",
+            "False",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "2"
+    },
+    {
+        question: "Which keyword is used to refer to properties or methods within the class itself?",
+        choices: [
+            "Private",
+            "Public",
+            "Protected",
+            "$this",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "4"
+    },
+    {
+        question: "How can we call the parent constructor in PHP?",
+        choices: [
+            "parent:__construct()",
+            "parent:construct()",
+            "parent::construct()",
+            "parent::__construct()",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "4"
+    },
+    {
+        question: "Which of the following is used to convert all applicable characters to HTML entities?",
+        choices: [
+            "html_entities",
+            "html_elements",
+            "htmlentities",
+            "htmlelements",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "3"
+    },
+    {
+        question: "Which of the following is the Scope Resolution Operator?",
+        choices: [
+            "\"",
+            "::",
+            ":",
+            "^",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "2"
+    },
+    {
+        question: "What keyword prevents a method from being redefined by a child class?",
+        choices: [
+            "abstract", 
+            "protected", 
+            "final", 
+            "static",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "3"
+    },
+    {
+        question: "What is the correct way to end a PHP statement?",
+        choices: [
+            ";",
+            ".",
+            "New line",
+            "None of the above",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "1"
+    },
+    {
+        question: "PHP server scripts are surrounded by delimiters, which?",
+        choices: [
+            "<?php...?>",
+            "<&>...</&>",
+            "<?php>...</?>",
+            "<script>...</script>",
+        ],
+        time: 30,
+        multiple: false,
+        answer: "1"
+    },
+];
+
+export { allQuestions };
---
+++ src/components/navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="section" id="user-section">
+        <header>
+            <nav>
+                <ul>
+                    <a href="#" style="text-decoration: none; color: white;"><h2>Quizes</h2></a>
+                </ul>
+                    <!-- <h2><?= $_SESSION['user_info']['username'] ?></h2> -->
+                    <h2>username</h2>
+                    <a id="logout-button" href="#">LOGOUT</a>
+            </nav>
+        </header>
+        <div class="" >
+            <div id="quizes" data-admin="0">
+                <!-- <?php
+                    require_once 'includes/functions.inc.php';
+                    show_quizes(false);
+                ?> -->
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+</script>
